5265969f409242e3783300fff586181e270b83fa
matthewlow04 <matthew7419@gmail.com>
2024-08-11 11:33:10 -0400

    Added scroll effect and updated some info

--- project.js
+++ project.js
@@ -29,6 +29,33 @@ const handleOnMove = e => {
   }
 };
 
+const handleOnScroll = e => {
+    e.preventDefault(); 
+  
+    const list = document.getElementById("image-list");
+    
+    const scrollDelta = e.deltaY;
+    
+    const maxDelta = window.innerWidth * 3; 
+    
+    const percentage = (scrollDelta / maxDelta) * -100;
+    
+    const nextPercentageUnconstrained = parseFloat(list.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    
+    list.dataset.percentage = nextPercentage;
+    list.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+    for (const image of list.getElementsByClassName("image")) {
+      image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+  
+    list.dataset.prevPercentage = nextPercentage;
+  };
+
+  window.addEventListener("wheel", handleOnScroll);
+  
+
 window.addEventListener("mousedown", e => handleOnDown(e));
 window.addEventListener("touchstart", e => handleOnDown(e.touches[0]));
 
@@ -38,3 +65,7 @@ window.addEventListener("touchend", e => handleOnUp(e.touches[0]));
 window.addEventListener("mousemove", e => handleOnMove(e));
 window.addEventListener("touchmove", e => handleOnMove(e.touches[0]));
 
+window.addEventListener("wheel", e => {
+    e.preventDefault();
+    handleOnScroll(e, true);
+});
